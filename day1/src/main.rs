248765e73a98a78e20da99e69611c8e9311500b0
Alexandre Sparton <sparton.alexandre@gmail.com>
2023-12-05 19:15:37 +0100

    refactor: playing a bit with rust language

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,8 +1,8 @@
-use std::fs;
+use std::{fs, io};
 
 fn main() {
     const FILE_PATH: &str = "resources/input.txt";
-    let inputs = read_file_to_str_list(FILE_PATH);
+    let inputs = read_file_to_str_list(FILE_PATH).unwrap();
     let mut calibaration_value_sum: u32 = 0;
     for input in inputs {
         calibaration_value_sum += get_number_from_line(&input);
@@ -10,28 +10,23 @@ fn main() {
     println!("{}", calibaration_value_sum);
 }
 
-fn read_file_to_str_list(file_path: &str) -> Vec<String> {
-    let input = fs::read_to_string(file_path).expect("Could not read file.");
-    let lines: Vec<&str> = input.split('\n').collect();
-    return lines.iter().map(|line| String::from(*line)).collect();
+fn read_file_to_str_list(file_path: &str) -> Result<Vec<String>, io::Error> {
+    Ok(fs::read_to_string(file_path)?
+        .split('\n')
+        .map(|line| String::from(line))
+        .collect())
 }
 
 fn get_number_from_line(line: &String) -> u32 {
     let only_numbers_line = extract_numbers_from_line(line);
-    let mut number_as_str = String::new();
-    number_as_str.push(
-        only_numbers_line
-            .chars()
-            .nth(0)
-            .expect("First index should be included."),
-    );
-    number_as_str.push(
-        only_numbers_line
+    (only_numbers_line.chars().nth(0).unwrap().to_string()
+        + &only_numbers_line
             .chars()
             .nth(only_numbers_line.len() - 1)
-            .expect("Last index should be included."),
-    );
-    number_as_str.parse().expect("Should be a number")
+            .unwrap()
+            .to_string())
+        .parse::<u32>()
+        .unwrap()
 }
 
 fn extract_numbers_from_line(line: &String) -> String {
